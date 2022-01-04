a4ba5e60aa790b0548603f8f9aa955b491b1a49b
fsmutimeer <ishpata@hotmail.com>
2022-01-04 22:15:55 +0500

    replacer_domain set

--- Automate/automate.js
+++ Automate/automate.js
@@ -8,15 +8,14 @@ var readyList = [];
 //Read and write to inmemory/list/domainList
 const readFile = async() => {
     try {
-        var text = fs.readFileSync('../redirects.txt', 'utf-8');
-        var textByLine = text.split('\n')
+        let domains = await fs.promises.readFile('../redirects.txt', 'utf-8');
+        domains = domains.split('\n');
+        // console.log(domains)
 
-        var textFile2 = fs.readFileSync('../replace.txt', 'utf-8');
-        var textByLine2 = textFile2.split('\n')
+        let replacerFile =await fs.promises.readFile('../replace.txt', 'utf-8');
+        replacerFile = replacerFile.split('\n')
 
-
-
-        getDomain(textByLine, textByLine2)
+        getDomain(domains, replacerFile)
 
     } catch (error) {
         console.log(error)
@@ -25,53 +24,42 @@ const readFile = async() => {
 
 //get the domain
 
-const getDomain = async(textByLine, textFile2) => {
+const getDomain = async(domains, replacerFile) => {
     try {
         // console.log(domainList[0].length);
-        var length = textByLine.length;
+        let length = domains.length;
         let urlPath;
         let newUrl;
+        let replacer;
+        let domain;
 
-        // for (let a = 0; a < textFile2.length; a++) {
-        //     replacer = textFile2[a];
-        //     // console.log(replacer);
-        //     for (let i = 0; i < textByLine.length; i++) {
-        //         var text = textByLine[i];
-        //         var text = text.replace(text, replacer);
-        //         console.log(text);
-        //     }
-        // }
+        for (let line = 0; line < length; line++) {
+            urlPath = domains[line]
+            urlPath = urlPath.split('=');
+       
+            urlPath.forEach(element => {
 
-        // var text = textByLine[0];
-        // var text = text.replace("www.lescapadealascaux.com", "ali@www.com");
-        // console.log(text);
-
-
-
-
-
-        // for (let line = 0; line < length; line++) {
-        //     console.log(textByLine[line]);
-        // }
-
-        // console.log("this is list index  0", domainList[0])
-        // for (let line = 0; line < length; line++) {
-        //     urlPath = domainList[line]
-        //     urlPath = urlPath.split('=');
-        //     // console.log(line, 'line =>', urlPath);
-        //     // console.log(urlPath);
-        //     urlPath.forEach(element => {
-
-        //         let ele = element ? element : ''
-        //         newUrl = url.parse(ele).hostname;
-        //         if (newUrl !== null) {
-        //             filteredList.push(newUrl)
-        //         }
-        //     });
-
-        // }
-        // console.log(filteredList);
-        // console.log('most frequent domain is ', findMostFrequent(filteredList));
+                let ele = element ? element : ''
+                newUrl = url.parse(ele).hostname;
+                if (newUrl !== null) {
+                    filteredList.push(newUrl)
+                }
+            });
+        }
+        //for loop for domain list
+        let newDomain;
+        let count = 0;
+        let replacerME=findMostFrequent(filteredList);
+        for(let j = 0; j < replacerFile.length; j++)
+        {
+            console.log(j)
+        }
+        for(let i = 0; i < length; i++)
+        {
+            let url = domains[i];
+            newDomain = url.replace(replacerME, 'ferozshahmutimeer.com');
+            // console.log(newDomain)
+        }
 
     } catch (error) {
         console.log(error)
@@ -83,10 +71,6 @@ function findMostFrequent(arr) {
     let mf = 1;
     let m = 0;
     let item;
-    let count = 0;
-    let result;
-    // console.log('array length is', arr.length)
-
     for (let i = 0; i < arr.length; i++) {
         for (let j = i; j < arr.length; j++) {
             if (arr[i] == arr[j]) {
@@ -94,20 +78,12 @@ function findMostFrequent(arr) {
                 if (m > mf) {
                     mf = m;
                     item = arr[i];
-                    domainList[0].map(function(domain) {
-                        result = (domain.replace(item, 'www.ferozshahmutimeer.com'));
-                        readyList.push(result)
-                    });
                 }
 
             }
         }
         m = 0;
     }
-    fs.writeFile('mynewtextfileferoz.txt', readyList.toString(), 'utf-8', function(err) {
-        if (err) return console.log(err);
-        console.log('writtng to new file, succeded')
-    })
     return item;
 }
 
